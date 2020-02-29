19fa349149b229819e9cbb4526b6b902772df120
ahrasel <ahrasel95@gmail.com>
2020-02-29 18:51:53 +0600

    Campaing Category and campaing resources addded

---
+++ database/factories/CampaignCategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\CampaignCategory;
+use Faker\Generator as Faker;
+
+$factory->define(CampaignCategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
